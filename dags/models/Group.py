47ec85929a270748a07953f97ac024622f8d44d7
a920604a <a920604a@gmail.com>
2024-12-11 17:22:04 +0800

    update init group

--- dags/models/Group.py
+++ dags/models/Group.py
@@ -3,17 +3,36 @@ from sqlalchemy.dialects.postgresql import ENUM, UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
-from utils.code_enum import partnerEducationStep_t
+from utils.code_enum import partnerEducationStep_t, group_type_t
 from .base import Base  # 引用分離出的 Base
+
 
 
 class Group(Base):
     __tablename__ = 'group'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     title = Column(Text)
-    group_type = Column(Enum(GroupTypeEnum), default='other')
-    area_id = Column(Integer, ForeignKey('location.id'))
-    created_by = Column(String, ForeignKey("users.uuid"))
+    photoURL = Column(String(255))
+    photoALT = Column(String(255))
+    category = Column(Text)
+    group_type = Column(Enum('other', name='group_type_t'), default='other')
+    partnerEducationStep = Column(Enum('other', name='partnerEducationStep_t'), default='other')
+    description = Column(String(255))
+    area_id = Column(Integer)
     isGrouping = Column(Boolean)
-    partnerEducationStep = Column(partnerEducationStep_t, default='other')
+    updatedDate = Column(Date)
+    time = Column(Time)
+    partnerStyle = Column(Text)
+    tagList = Column(Text)
+    created_at = Column(Date)
+    created_by = Column(UUID)
+    updated_at = Column(Date)
+    updated_by = Column(String(255))
+    motivation = Column(Text)
+    Contents = Column(Text)
+    expectation_result = Column(Text)
+    Notice = Column(Text)
+    group_daedline = Column(Date)
+    hold_time = Column(Time)
+    isOnline = Column(Boolean)
+    TBD = Column(Boolean)
---
+++ dags/models/OldActivities.py
@@ -0,0 +1,29 @@
+from sqlalchemy import (
+    Column, Integer, String, Text, Boolean, Date, Time, ForeignKey, Enum
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
+
+Base = declarative_base()
+
+class OldActivities(Base):
+    __tablename__ = 'old_activities'
+    _id = Column(Text, primary_key=True)
+    userId = Column(Text)
+    title = Column(Text)
+    photoURL = Column(Text)
+    photoAlt = Column(Text)
+    category = Column(Text)
+    area = Column(Text)
+    time = Column(Text)
+    partnerStyle = Column(Text)
+    partnerEducationStep = Column(Text)
+    description = Column(Text)
+    tagList = Column(Text)
+    isGrouping = Column(Boolean)
+    createdDate = Column(Text)
+    updatedDate = Column(Text)
+    created_at = Column(Text)
+    created_by = Column(Text)
+    updated_at = Column(Text)
+    updated_by = Column(Text)
