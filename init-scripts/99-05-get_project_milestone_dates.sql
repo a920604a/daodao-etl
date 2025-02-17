df4a2dcd33ed588a83f74b3ae53db8c3781c1b4b
a920604a <a920604a@gmail.com>
2025-02-17 10:22:24 +0800

    add pq method get_project_milestone_dates

---
+++ init-scripts/99-05-get_project_milestone_dates.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION public.get_project_milestone_dates(p_project_id integer DEFAULT NULL::integer)
+RETURNS TABLE(
+    project_id integer, 
+    project_title text, 
+    milestone_start_date date, 
+    milestone_end_date date, 
+    week_number integer,
+    is_completed boolean 
+)
+LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+    SELECT
+        p.id AS project_id,  
+        p.title::TEXT AS project_title,
+        m.start_date AS milestone_start_date,
+        m.end_date AS milestone_end_date,
+        ((EXTRACT(DAY FROM (m.start_date::timestamp - (SELECT MIN(m2.start_date) FROM public.milestone m2 WHERE m2.project_id = p.id))) / 7) + 1)::integer AS week_number,
+        m.is_completed 
+    FROM
+        public.project p
+    JOIN
+        public.milestone m ON p.id = m.project_id
+    WHERE
+        (p_project_id IS NULL OR p.id = p_project_id)  
+    ORDER BY
+        p.id, m.start_date;
+END;
+$function$;
